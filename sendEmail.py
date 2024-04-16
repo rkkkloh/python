8d18123206f9f338eefd91047f742641c7047ce0
Rui Kang Loh <ruikangloh@Ruis-MacBook-Air.local>
2024-04-16 16:35:09 +0800

    Implementing program bsaed on learning progress && Create first executable .app file in macOs

---
+++ sendEmail.py
@@ -0,0 +1,26 @@
+import smtplib
+
+sender = "ruikangloh@gmail.com"
+receiver = "d84127099@gs.ncku.edu.tw"
+password = "passwordfordemonstartion"
+subject = "Python email test"
+body = "I wrote an email! :D"
+
+# header
+message = f"""From: Snoop Dogg{sender}
+To: Nicholas Cage{receiver}
+Subject: {subject}\n
+{body}
+"""
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
+server.starttls()
+
+try:
+    server.login(sender,password)
+    print("Logged in...")
+    server.sendmail(sender, receiver, message)
+    print("Email has been sent!")
+
+except smtplib.SMTPAuthenticationError:
+    print("unable to sign in")
